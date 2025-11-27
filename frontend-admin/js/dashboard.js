ee8d29ab95f76447b7f4f69706bd24ded6bbb6e4
hyukkyu <81544639+hyukkyu@users.noreply.github.com>
2025-11-27 20:56:29 +0900

    Update dashboard.js

--- frontend-admin/js/dashboard.js
+++ frontend-admin/js/dashboard.js
@@ -52,7 +52,7 @@ let notices = [];
 
 async function fetchJson(path, options = {}) {
   const response = await fetch(`${API_BASE}${path}`, {
-    credentials: 'include',
+    credentials: 'include', #asdf
     headers: {
       'Content-Type': 'application/json',
       ...(options.headers || {})
